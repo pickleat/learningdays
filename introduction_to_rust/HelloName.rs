baed6f1054e08b503aafd08fce087dcf9f02acd2
Andy Pickle <pickleat@gmail.com>
2022-02-17 17:18:43 -0600

    adding code from introduction to rust

---
+++ introduction_to_rust/HelloName.rs
@@ -0,0 +1,7 @@
+fn hello_name(name: &str) {
+  println!("Hello {}!", name);
+}
+
+fn main() {
+  hello_name("Andy");
+}
---
+++ introduction_to_rust/Return12.rs
@@ -0,0 +1,7 @@
+fn main() {
+  let x = return_twelve();
+  println!("{}", x);
+}
+fn return_twelve() -> i32 {
+  12
+}
